c8b2144cbabeb8e3922012d4a099e0abaaa78c3a
Erik Lövbom <erik.lovbom@studentconsulting.com>
2016-06-30 14:37:35 +0200

    Första uppdateringen

---
+++ Garage2.0Erik/Models/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage2._0Erik.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public string RegNr { get; set; }
+        
+        public string Owner  { get; set; }
+        
+        public EColor Color { get; set; }
+        
+        public int NumberOfWheels { get; set; }
+
+        public VehicleType Type { get; set; }
+    }
+
+    public enum VehicleType
+    {
+        Car,
+        Truck, 
+        Bus,
+        Boat,
+        Motorcycle
+    }
+
+    public enum EColor
+    {
+        White,
+        Grey,
+        Yellow,
+        Red,
+        Blue,
+        Green,
+        Black
+
+    }
+}
---
+++ Garage2.0Erik/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Garage2._0Erik.Startup))]
+namespace Garage2._0Erik
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
